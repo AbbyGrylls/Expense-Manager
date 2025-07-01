6d49b43818e6ef782f438afece91ea8fe6aec9b1
Abhishek Kumar (Absie) <113328106+AbbyGrylls@users.noreply.github.com>
2025-07-01 15:24:25 +0530

    Update axios.js

--- api/axios.js
+++ api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 const instance = axios.create({
-  baseURL: 'http://10.31.25.203:8000',
+  baseURL: 'http://:8000',//add your frontend ip
   timeout: 10000,
 });
 instance.interceptors.request.use(
